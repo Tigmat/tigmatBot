012338e33ec610342783e987d6981599da9f444d
Tigmat <matuskud@gmail.com>
2018-05-17 18:38:38 +0200

    first commit

---
+++ tigmatBot.py
@@ -0,0 +1,38 @@
+#tigmatBot by Tigmat Gaming
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+
+bot = commands.Bot(command_prefix='#')
+
+@bot.event
+async def on_ready():
+    print("I am running on: " + bot.user.name)
+    print("ID=" + bot.user.id)
+
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+    embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find.", color=0xff0000)
+    embed.add_field(name="Name", value=user.name, inline=True)
+    embed.add_field(name="Status", value=user.status, inline=True)
+    embed.add_field(name="Highest role", value=user.top_role)
+    embed.add_field(name="Joined", value=user.joined_at)
+    embed.set_thumbnail(url=user.avatar_url)
+    await bot.say(embed=embed)
+
+@bot.command(pass_context=True)
+async def serverinfo(ctx):
+    embed = discord.Embed(name="{}'s info".format(ctx.message.server.name), description="Here's what I could find.", color=0xff0000)
+    embed.set_author(name="Made by TigmatGaming")
+    embed.add_field(name="Name", value=ctx.message.server.name, inline=True)
+    embed.add_field(name="Roles", value=len(ctx.message.server.roles), inline=True)
+    embed.add_field(name="Members", value=len(ctx.message.server.members))
+    embed.set_thumbnail(url=ctx.message.server.icon_url)
+    await bot.say(embed=embed)
+
+
+
+bot.run("NDQ0NzQyNTQ4OTIyMDQwMzIw.Ddy6Qg.0pxp-YMLoCOkdGMwMt74lytboR4")
+
